2b6b59230ee0361454dacce8e119174955ee532c
Katherine Wang <90437215+kcw27@users.noreply.github.com>
2023-10-17 14:03:02 -0700

    Update class_oct12.py
    Made use of the new feature of sorting mistake frequency dictionaries by mistake frequency rather than alphabetically.

--- tutoring/class_oct12.py
+++ tutoring/class_oct12.py
@@ -21,6 +21,12 @@ for test in all_tests:
 	print(test, "mistakes without filtering: length of", len(whole), "\n", whole)
 	print(test, "mistakes with threshold of 2: length of", len(filtered), "\n", filtered)
 
+	# also, to test out the parameter that specifies whether to sort alphabetically or by frequency:
+	whole_byfreq = mistakes.mistake_freqs_d(all_tests[test], alphabetical = False)
+	filtered_byfreq = mistakes.common_mistakes(all_tests[test], 2, alphabetical = False) # filter to only mistakes made by 2+ people
+	print(test, "mistakes without filtering, by frequency: length of", len(whole_byfreq), "\n", whole_byfreq)
+	print(test, "mistakes with threshold of 2, by frequency: length of", len(filtered_byfreq), "\n", filtered_byfreq)
+
 	if len(whole) <= 20: # assuming that a list of over 20 mistakes is too many to review
 		print("Human-readable complete list:")
 		[print(i) for i in whole]
